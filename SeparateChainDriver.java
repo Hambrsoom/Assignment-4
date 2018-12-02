682cf7fdf62fba6cb3757a74aaf1b05fb6688039
Mohanad Arafe <34899555+mohanadarafe@users.noreply.github.com>
2018-12-02 13:48:24 -0500

    Update SeparateChainDriver.java

--- SeparateChainDriver.java
+++ SeparateChainDriver.java
@@ -4,16 +4,29 @@ public class SeparateChainDriver{
 
 	public static void main(String[] args){
 
-		SeperateChain seperateChain = new SeperateChain(100);
+		SeparateChain separateChain = new SeparateChain(100);
 		Random random = new Random();
 		int[] keys = new int[50];
 		
 		//Putting 50 values in the table
+		System.out.println("---------PUT(K,V) 50 VALUES-------");
 		for(int i = 0; i < keys.length; i++){
 			int key = random.nextInt();
 			keys[i] = key;
 			String value   = "word_"+i;
-	      seperateChain.put(key, value);
+	      separateChain.put(key, value);
+		}
+
+		//Getting 50 values from the table
+		System.out.println("---------GET(K) 50 VALUES-------");
+		for(int i = 0; i < keys.length; i++){
+			System.out.println(separateChain.get(keys[i]));
+		}
+
+		//Remove first 25 values
+		System.out.println("---------REMOVE(K) 25 VALUES-------");
+		for(int i = 0; i < 25; i++){
+			System.out.println(separateChain.remove(keys[i]));
 		}
 
 	}
